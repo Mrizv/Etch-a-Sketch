a661fdc91c4466af36386baf3f51886e21a752bd
Maarij Rizvi <148887640+Mrizv@users.noreply.github.com>
2025-07-22 01:44:27 -0500

    Create 16x16 grid of square divs, wrapped with flexbox

---
+++ EAS.js
@@ -0,0 +1,7 @@
+const grid = document.getElementById("squares");
+
+for (let i = 0; i < 256; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  grid.appendChild(square);
+}
