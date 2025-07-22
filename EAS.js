5161ad32698481a3d4684a3dd2f0b08ce9687305
Maarij Rizvi <148887640+Mrizv@users.noreply.github.com>
2025-07-22 02:00:12 -0500

    Grid now responds to mouse hover & non-functional "Clear Grid" button added

--- EAS.js
+++ EAS.js
@@ -4,4 +4,8 @@ for (let i = 0; i < 256; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
   grid.appendChild(square);
+
+  square.addEventListener("mouseenter", () => {
+    square.style.backgroundColor = "blue";
+  });
 }
